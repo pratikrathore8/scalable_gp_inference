82880a4bcf6cac84535917b82cd72a61854c6093
Pratik Rathore <pratikr@stanford.edu>
2025-04-10 16:51:24 -0700

    checkpoint

--- scalable_gp_inference/kernel_linsys.py
+++ scalable_gp_inference/kernel_linsys.py
@@ -45,6 +45,7 @@ class KernelLinSys(LinSys):
             devices (Optional[Set[torch.device]]): Set of devices for
             distributed computation. Defaults to None.
         """
+        # Set up superclass
         kernel_linop = self._get_kernel_linop(
             X, kernel_type, kernel_lengthscale, distributed, devices
         )
@@ -55,7 +56,14 @@ class KernelLinSys(LinSys):
             A_row_oracle=kernel_linop.row_oracle,
             A_blk_oracle=kernel_linop.blk_oracle,
         )
+        # Figure out which columns of B to track for termination
         self.residual_tracking_idx = residual_tracking_idx
+        self.B_eval = (
+            B if residual_tracking_idx is None else B[:, self.residual_tracking_idx]
+        )
+        self.B_eval = (
+            self.B_eval.to_dense() if isinstance(B, GPInferenceRHS) else self.B_eval
+        )
 
     def _get_kernel_linop(
         self,
@@ -84,3 +92,25 @@ class KernelLinSys(LinSys):
         if distributed:
             linop_kwargs.update({"devices": devices})
         return linop_class(**linop_kwargs)
+
+    def _compute_internal_metrics(self, W: torch.Tensor):
+        W_in = (
+            W[:, self.residual_tracking_idx]
+            if self.residual_tracking_idx is not None
+            else W
+        )
+        abs_res = torch.linalg.norm(
+            self.B_eval - (self.A @ W_in + self.reg * W_in), dim=0, ord=2
+        )
+        rel_res = abs_res / torch.linalg.norm(self.B_eval, dim=0, ord=2)
+        return {"abs_res": abs_res, "rel_res": rel_res}
+
+    def _check_termination_criteria(
+        self, internal_metrics: dict, atol: float, rtol: float
+    ):
+        abs_res = internal_metrics["abs_res"]
+        comp_tol = torch.clamp(
+            rtol * torch.linalg.norm(self.B_eval, dim=0, ord=2), min=atol
+        )
+        self._mask = abs_res > comp_tol
+        return (~self._mask).all().item()
