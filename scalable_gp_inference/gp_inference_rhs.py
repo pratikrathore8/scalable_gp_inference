ca89a4bbf0a3b389ba04c35c5e41c70cb4c492a6
Pratik Rathore <pratikr@stanford.edu>
2025-04-10 16:11:43 -0700

    checkpoint

--- scalable_gp_inference/gp_inference_rhs.py
+++ scalable_gp_inference/gp_inference_rhs.py
@@ -5,11 +5,15 @@ import torch
 
 class GPInferenceRHS(CatLinearOperator):
     def __init__(
-        self, targets: torch.Tensor, X: torch.Tensor, K: Kernel, tst_idx: torch.Tensor
+        self,
+        targets: torch.Tensor,
+        X: torch.Tensor,
+        kernel_fn: Kernel,
+        tst_idx: torch.Tensor,
     ):
         if targets.shape[0] != X.shape[0]:
             raise ValueError("The number of rows in targets and X must match.")
 
         targets_linop = DenseLinearOperator(targets)
-        X_tst_kernel_linop = K(X, X[tst_idx])
+        X_tst_kernel_linop = kernel_fn(X, X[tst_idx])
         super().__init__(targets_linop, X_tst_kernel_linop, dim=1)
