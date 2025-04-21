00b78e627653ec2ff85edd6c2092ea78bad206c5
Pratik Rathore <pratikr@stanford.edu>
2025-04-21 12:16:06 -0700

    add negative log likelihood calculations

--- scalable_gp_inference/gp_inference.py
+++ scalable_gp_inference/gp_inference.py
@@ -1,7 +1,8 @@
 from typing import Optional, Set, Union
 
-import torch
+
 from rlaopt.solvers import SolverConfig
+import torch
 
 from .kernel_linsys import KernelLinSys
 from .random_features import get_random_features
@@ -23,6 +24,7 @@ class GPInference:
         num_posterior_samples: Optional[int] = 0,
         num_random_features: Optional[int] = 0,
     ):
+        # NOTE(pratik): this class assumes a zero-mean GP prior
         self.Xtr = Xtr
         self.ytr = ytr
         self.Xtst = Xtst
@@ -97,17 +99,59 @@ class GPInference:
             devices=self.devices,
         )
 
+    def _compute_nll(means: torch.Tensor, variances: torch.Tensor, locs: torch.Tensor):
+        n = means.shape[0]
+        log_variances = torch.log(variances)
+        nll = 0.5 * (
+            (locs - means) ** 2 / variances
+            + torch.sum(log_variances)
+            + n * torch.log(torch.tensor(2 * torch.pi))
+        )
+        return nll
+
     def _callback_fn(self, W: torch.Tensor, linsys: KernelLinSys, tst_kernel_linop):
         train_rmse = torch.sqrt(
-            1 / self.ytr.shape[0] * torch.sum((self.ytr - linsys.A @ W) ** 2)
+            1 / self.ytr.shape[0] * torch.sum((self.ytr - linsys.A @ W[:, 0]) ** 2)
         )
+        test_mean = tst_kernel_linop @ W[:, 0]
         test_rmse = torch.sqrt(
-            1 / self.ytst.shape[0] * torch.sum((self.ytst - tst_kernel_linop @ W) ** 2)
+            1 / self.ytst.shape[0] * torch.sum((self.ytst - test_mean) ** 2)
         )
-        return {
+        metrics_dict = {
             "train_rmse": train_rmse.cpu().item(),
             "test_rmse": test_rmse.cpu().item(),
+            "test_mean": test_mean.cpu().numpy(),
         }
+
+        # Compute variances and negative log likelihood using posterior samples
+        if self.Xtr_prior_samples is not None and self.Xtst_prior_samples is not None:
+            W_diff = _safe_unsqueeze(W[:, 0]) - W[:, 1:]
+            test_posterior_samples = self.Xtst_prior_samples + tst_kernel_linop @ W_diff
+            test_posterior_samples_mean = test_posterior_samples.mean(
+                dim=1
+            )  # Useful for sanity checking with test_mean
+            test_posterior_samples_var = test_posterior_samples.var(dim=1)
+            test_posterior_samples_nll = self._compute_nll(
+                means=test_posterior_samples_mean,
+                variances=test_posterior_samples_var,
+                locs=self.ytst,
+            )
+            test_posterior_samples_mean_nll = (
+                test_posterior_samples_nll / self.Xtst.shape[0]
+            )
+            mean_value = test_posterior_samples_mean.cpu().numpy()
+            var_value = test_posterior_samples_var.cpu().numpy()
+            nll_value = test_posterior_samples_nll.cpu().item()
+            mean_nll_value = test_posterior_samples_mean_nll.cpu().item()
+            metrics_dict.update(
+                {
+                    "test_posterior_samples_mean": mean_value,
+                    "test_posterior_samples_var": var_value,
+                    "test_posterior_samples_nll": nll_value,
+                    "test_posterior_samples_mean_nll": mean_nll_value,
+                }
+            )
+        return metrics_dict
 
     def perform_inference(
         self,
@@ -117,13 +161,13 @@ class GPInference:
         log_in_wandb: Optional[bool] = False,
         wandb_init_kwargs: Optional[dict] = {},
     ):
-        training_linsys = self._get_linsys()
+        linsys = self._get_linsys()
         tst_kernel_linop = self._get_tst_kernel_linop()
 
         if W_init is None:
-            W_init = torch.zeros_like(training_linsys.B)
+            W_init = torch.zeros_like(linsys.B)
 
-        solution, log = training_linsys.solve(
+        solution, log = linsys.solve(
             solver_config=solver_config,
             W_init=W_init,
             callback_fn=self._callback_fn,
