277424496b30c4fc7949c4debbcdb4458813e40e
Pratik Rathore <pratikr@stanford.edu>
2025-04-23 13:00:34 -0700

    add use_full_kernel option to KernelLinSys

--- scalable_gp_inference/kernel_linsys.py
+++ scalable_gp_inference/kernel_linsys.py
@@ -18,6 +18,7 @@ class KernelLinSys(LinSys):
         residual_tracking_idx: torch.Tensor | None = None,
         distributed: bool = False,
         devices: set[torch.device] | None = None,
+        use_full_kernel: bool = True,
     ):
         """Initialize KernelLinSys model.
 
@@ -34,6 +35,8 @@ class KernelLinSys(LinSys):
             Defaults to False.
             devices (set[torch.device] | None): Set of devices to use for distributed
             computation. Defaults to None.
+            use_full_kernel (bool): Whether to use the full kernel. Defaults to True.
+            If False, then residuals will be set to infinity.
         """
         # Set up superclass
         kernel_linop = _get_kernel_linop(
@@ -51,18 +54,36 @@ class KernelLinSys(LinSys):
         self.B_eval = (
             B if residual_tracking_idx is None else B[:, self.residual_tracking_idx]
         )
+        self.use_full_kernel = use_full_kernel
 
     def _compute_internal_metrics(self, W: torch.Tensor):
-        W_in = (
-            W[:, self.residual_tracking_idx]
-            if self.residual_tracking_idx is not None
-            else W
-        )
-        abs_res = torch.linalg.norm(
-            self.B_eval - (self.A @ W_in + self.reg * W_in), dim=0, ord=2
-        )
-        rel_res = abs_res / torch.linalg.norm(self.B_eval, dim=0, ord=2)
-        return {"abs_res": abs_res, "rel_res": rel_res}
+        if self.use_full_kernel:
+            W_in = (
+                W[:, self.residual_tracking_idx]
+                if self.residual_tracking_idx is not None
+                else W
+            )
+            abs_res = torch.linalg.norm(
+                self.B_eval - (self.A @ W_in + self.reg * W_in), dim=0, ord=2
+            )
+            rel_res = abs_res / torch.linalg.norm(self.B_eval, dim=0, ord=2)
+            return {"abs_res": abs_res, "rel_res": rel_res}
+
+        # If not using full kernel, set residuals to infinity
+        return {
+            "abs_res": torch.full(
+                self.B_eval.shape[1],
+                float("inf"),
+                dtype=self.B_eval.dtype,
+                device=self.B_eval.device,
+            ),
+            "rel_res": torch.full(
+                self.B_eval.shape[1],
+                float("inf"),
+                dtype=self.B_eval.dtype,
+                device=self.B_eval.device,
+            ),
+        }
 
     def _check_termination_criteria(
         self, internal_metrics: dict, atol: float, rtol: float
